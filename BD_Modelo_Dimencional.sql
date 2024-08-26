6ba4935382aad4da6ad0d5ce44b0d081cef09626
Hamnah <20210841@lamolina.edu.pe>
2024-08-26 16:08:06 -0500

    Trabajo_4_modelo dimencional

--- BD_Modelo_Dimencional.sql
+++ BD_Modelo_Dimencional.sql
@@ -4,51 +4,47 @@ GO
 -- Seleccion BD
 USE Asistencia_Academia_Taekwondo_BI;
 
--- Creación de tablas:
 CREATE TABLE dim_Estudiantes(
 ID_Estudiante INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 Nombre VARCHAR(250) NOT NULL,
 Apellido_Paterno VARCHAR(100) NOT NULL,
 Apellido_Materno VARCHAR(100) NOT NULL,
 Fecha_Nacimiento DATE NOT NULL,
-Genero VARCHAR NULL,
-Fecha_Registro DATE NOT NULL
+Fecha_Registro DATE NOT NULL,
+Numero_Documento VARCHAR(50) UNIQUE NULL
 );
 
 CREATE TABLE dim_Instructor(
-ID_Instructor INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-Nombre VARCHAR(200) NOT NULL,
-Apellido_Paterno VARCHAR(100) NOT NULL,
-Apellido_Materno VARCHAR(100) NOT NULL,
-Especialidad VARCHAR(100) NULL,
-Cinturon VARCHAR(100) NULL
+    ID_Instructor INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    Nombre VARCHAR(200) NOT NULL,
+    Apellido_Paterno VARCHAR(100) NOT NULL,
+    Apellido_Materno VARCHAR(100) NOT NULL,
 );
 
-CREATE TABLE dim_Clase(
-ID_Clase INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-Nombre_Clase VARCHAR(150) NOT NULL,
-Nivel VARCHAR(300) null,
-DíaS_Semana VARCHAR(100) NOT NULL,
-Horario TIME NOT NULL,
 
+CREATE TABLE dim_Clase (
+    ID_Clase INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    Nombre_Clase VARCHAR(150) NOT NULL,
+    Día_Semana VARCHAR(100) NOT NULL,
+    Horario TIME NOT NULL,
+    ID_Instructor INT NOT NULL,
+    FOREIGN KEY (ID_Instructor) REFERENCES dim_Instructor(ID_Instructor)
 );
 
-CREATE TABLE dim_tiempo(
-	id_fecha DATE PRIMARY KEY NOT NULL,
-	Día INT NOT NULL,
-	Mes INT NOT NULL,
-	Dia_semana VARCHAR(25) NOT NULL,
-	Año INT NOT NULL
+CREATE TABLE dim_tiempo (
+    id_fecha DATE PRIMARY KEY NOT NULL,
+    Día INT NOT NULL,
+    Mes INT NOT NULL,
+    Dia_Semana VARCHAR(25) NOT NULL,
+    Año INT NOT NULL
 );
 
-CREATE TABLE dim_Asistencia(
-	id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	ID_Estudiante INT,
-	ID_Clase INT,
-	ID_Instructor INT, 
-	Fecha DATE, 
-	FOREIGN KEY (ID_Estudiante) REFERENCES dim_Estudiantes(ID_Estudiante),
-	FOREIGN KEY (ID_Clase) REFERENCES dim_clase(ID_Clase),
-	FOREIGN KEY (ID_Instructor) REFERENCES dim_Instructor(ID_Instructor),
-	FOREIGN KEY (Fecha) REFERENCES dim_tiempo(id_fecha)
+CREATE TABLE fact_Asistencia (
+    ID_Asistencia INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    ID_Estudiante INT NOT NULL,
+    ID_Clase INT NOT NULL,
+    ID_Fecha DATE NOT NULL,
+    FOREIGN KEY (ID_Estudiante) REFERENCES dim_Estudiantes(ID_Estudiante),
+    FOREIGN KEY (ID_Clase) REFERENCES dim_Clase(ID_Clase),
+    FOREIGN KEY (ID_Fecha) REFERENCES dim_tiempo(id_fecha)
 );
---
+++ Consultas SQL Para poblar el modelo dimensional.sql
@@ -0,0 +1,39 @@
+USE Asistencia_Academia_Taekwondo
+
+-- Consulta para poblar dim_Estudiantes
+
+select * from Estudiantes;
+
+SELECT ID_Estudiante as Id_Estudiante,
+		Nombre,Apellido_paterno,Apellido_Materno,
+		Fecha_Nacimiento, Fecha_Registro,Numero_Documento
+From Estudiantes;
+
+
+-- Consulta para poblar dim_instructor
+
+SELECT Nombre, Apellido_Paterno,Apellido_Materno
+FROM Instructor
+
+-- Consulta para poblar dim_clase
+
+SELECT ID_Clase,Nombre_Clase, Día_Semana,
+		CONCAT(Hora_Inicio, ' ' ,Hora_Fin) AS Horario,
+		Instructor_ID AS ID_Instructor
+FROM Clase
+
+-- Consulta para poblar dim_tiempo
+
+SET LANGUAGE Spanish;
+SELECT 
+    DISTINCT 
+	Fecha AS id_fecha,
+    DAY(Fecha) AS 'dia',
+	DATENAME(WEEKDAY, Fecha) AS 'dia_semana',
+    DATENAME(MONTH, Fecha) AS 'mes',
+    YEAR(Fecha) AS 'anio'
+FROM Asistencia;
+
+
+
+-- Consulta para poblar fact_asistencia
